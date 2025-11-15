375d37052f37e80e9e8799192bb2d2d0ddecf1fb
Rahul Verma <vermarahul7344@gmail.com>
2025-11-15 11:40:23 +0530

    f3.0

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,8 @@ const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json({ limit: "5mb" })); // req.body
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
